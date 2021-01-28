6cf9e0a6371e29de6ef0809ad001d881e562f0ad
Fábio <fabiodu465@gmail.com>
2021-01-28 12:30:38 +0000

    toques finais

--- app.py
+++ app.py
@@ -104,172 +104,173 @@ def index():
 
 @app.route('/list_sala')
 def list_sala():
-    r = Room.query.all()
-    return render_template('/list.html', data=r)
-
-
-@app.route('/search_sala', methods=['GET'])
-def all_sala():
-    r = Room.query.all()
-    names = [room.name for room in r]
-    return jsonify(names)
+    if 'username' in session:
+        r = Room.query.all()
+        return render_template('/list.html', data=r)
+    return redirect(url_for('admin_login'))
 
 
 @app.route('/list_user')
 def list_user():
-    u = User.query.all()
-    return render_template('/listuser.html', data=u)
-
+    if 'username' in session:
+        u = User.query.all()
+        return render_template('/listuser.html', data=u)
+    return redirect(url_for('admin_login'))
 
 @app.route('/insert_user')
 def insert_user():
-    u = User.query.all()
-    return render_template('adduser.html')
-
+    if 'username' in session:
+        u = User.query.all()
+        return render_template('adduser.html')
+    return redirect(url_for('admin_login'))
 
 @app.route('/insert_user2', methods=['POST'])
 def insert_user2():
-    nome = request.form['nome']
-    email = request.form['email']
-    password = request.form['password']
+    if 'username' in session:
+        nome = request.form['nome']
+        email = request.form['email']
+        password = request.form['password']
 
-    new_user = User(nome=nome, email=email, password=password)
+        new_user = User(nome=nome, email=email, password=password)
 
-    db.session.add(new_user)
-    db.session.commit()
+        db.session.add(new_user)
+        db.session.commit()
 
-    return redirect(url_for('list_user'))
+        return redirect(url_for('list_user'))
+    return redirect(url_for('admin_login'))    
 
 
 @app.route('/insert_sala')
 def insert_sala():
-    # hor = request.files['horario']
-    # return hor.filename
-    r = Room.query.all()
-    return render_template('addsala.html')
-
+    if 'username' in session:
+        r = Room.query.all()
+        return render_template('addsala.html')
+    return redirect(url_for('admin_login'))
 
 @app.route('/insert_sala2', methods=['POST'])
 def insert_sala2():
-    name = request.form['name']
-    capacity = request.form['capacity']
-    room_type = request.form['type']
-    floor_id = request.form['piso']
-    info = request.form['info']
-    map_position_x = request.form['x_pos']
-    map_position_y = request.form['y_pos']
-    schedule_file = request.files['horario']
-    filename = 'static/img/dummy.pdf'
+    if 'username' in session:
+        name = request.form['name']
+        capacity = request.form['capacity']
+        room_type = request.form['type']
+        floor_id = request.form['piso']
+        info = request.form['info']
+        map_position_x = request.form['x_pos']
+        map_position_y = request.form['y_pos']
+        schedule_file = request.files['horario']
+        filename = 'static/img/dummy.pdf'
 
-    if request.method == 'POST' and schedule_file:
-        filename = files.save(request.files['horario'])
+        if request.method == 'POST' and schedule_file:
+            filename = files.save(request.files['horario'])
 
-    new_room = Room(name=name, capacity=capacity, map_position_x=map_position_x, map_position_y=map_position_y,
-                    room_type=room_type, info=info, floor_id=floor_id, schedule_file=filename)
+        new_room = Room(name=name, capacity=capacity, map_position_x=map_position_x, map_position_y=map_position_y,
+                        room_type=room_type, info=info, floor_id=floor_id, schedule_file=filename)
 
-    db.session.add(new_room)
-    db.session.commit()
+        db.session.add(new_room)
+        db.session.commit()
 
-    return redirect(url_for('list_sala'))
+    return redirect(url_for('admin_login'))
 
 
 @app.route('/delete_sala', methods=['POST'])
 def delete_sala():
-    data = request.args.get('id_sala')
-    # delete_room = Room(id=id_room)
-    data_file = str(data) + '.pdf'
-    Room.query.filter_by(id=data).delete()
+    if 'username' in session:
+        data = request.args.get('id_sala')
+        # delete_room = Room(id=id_room)
+        data_file = str(data) + '.pdf'
+        Room.query.filter_by(id=data).delete()
 
-    my_file = Path(os.path.join(app.config['UPLOADED_FILES_DEST'], data_file))
-    if my_file.exists():
-        os.remove(os.path.join(app.config['UPLOADED_FILES_DEST'], data_file))
-    db.session.commit()
-    return redirect(url_for('list_sala'))
-
+        my_file = Path(os.path.join(app.config['UPLOADED_FILES_DEST'], data_file))
+        if my_file.exists():
+            os.remove(os.path.join(app.config['UPLOADED_FILES_DEST'], data_file))
+        db.session.commit()
+        return redirect(url_for('list_sala'))
+    return redirect(url_for('admin_login'))
 
 @app.route('/delete_user', methods=['POST'])
 def delete_user():
-    data = request.args.get('id_user')
-    User.query.filter_by(id=data).delete()
-    db.session.commit()
-    return redirect(url_for('list_user'))
-
+    if 'username' in session:
+        data = request.args.get('id_user')
+        User.query.filter_by(id=data).delete()
+        db.session.commit()
+        return redirect(url_for('list_user'))
+    return redirect(url_for('admin_login'))
 
 @app.route("/update_user", methods=['POST'])
 def update_user():
-    data = request.args.get('id_user')
-    update = User.query.filter_by(id=data)
+    if 'username' in session:    
+        data = request.args.get('id_user')
+        update = User.query.filter_by(id=data)
 
-    return render_template('edituser.html', update=update)
-
+        return render_template('edituser.html', update=update)
+    return redirect(url_for('admin_login'))
 
 @app.route('/edit_user', methods=['POST'])
 def edit_user():
-    data = request.form['id']
-    update = User.query.filter_by(id=data).first()
-    bd_doc = update.schedule_file
-    schedule_file = request.files['horario']
+    if 'username' in session:        
+        data = request.form['id']
+        update = User.query.filter_by(id=data).first()
+        bd_doc = update.schedule_file
+        schedule_file = request.files['horario']
 
-    if request.method == 'POST' and 'nome' in request.form:
-        update.nome = request.form['nome']
+        if request.method == 'POST' and 'nome' in request.form:
+            update.nome = request.form['nome']
 
-    if request.method == 'POST' and 'email' in request.form:
-        update.email = request.form['email']
+        if request.method == 'POST' and 'email' in request.form:
+            update.email = request.form['email']
 
-    if request.method == 'POST' and 'password' in request.form:
-        update.password = request.form['password']
+        if request.method == 'POST' and 'password' in request.form:
+            update.password = request.form['password']
 
-    db.session.commit()
-    return redirect(url_for('list_user'))
-
+        db.session.commit()
+        return redirect(url_for('list_user'))
+    return redirect(url_for('admin_login'))
 
 @app.route("/update_sala", methods=['POST'])
 def update_sala():
-    data = request.args.get('id_sala')
-    update = Room.query.filter_by(id=data).first()
+    if 'username' in session:   
+        data = request.args.get('id_sala')
+        update = Room.query.filter_by(id=data).first()
 
-    return render_template('editsala.html', update=update)
-
+        return render_template('editsala.html', update=update)
+    return redirect(url_for('admin_login')) 
 
 @app.route('/edit_sala', methods=['POST'])
 def edit_sala():
-    data = request.form['id']
-    update = Room.query.filter_by(id=data).first()
-    bd_doc = update.schedule_file
-    schedule_file = request.files['horario']
+    if 'username' in session:  
+        data = request.form['id']
+        update = Room.query.filter_by(id=data).first()
+        bd_doc = update.schedule_file
+        schedule_file = request.files['horario']
 
-    if request.method == 'POST' and 'name' in request.form:
-        update.name = request.form['name']
+        if request.method == 'POST' and 'name' in request.form:
+            update.name = request.form['name']
 
-    if request.method == 'POST' and 'capacity' in request.form:
-        update.capacity = request.form['capacity']
+        if request.method == 'POST' and 'capacity' in request.form:
+            update.capacity = request.form['capacity']
 
-    if request.method == 'POST' and 'x_pos' in request.form:
-        update.map_position_x = request.form['x_pos']
+        if request.method == 'POST' and 'x_pos' in request.form:
+            update.map_position_x = request.form['x_pos']
 
-    if request.method == 'POST' and 'y_pos' in request.form:
-        update.map_position_y = request.form['y_pos']
+        if request.method == 'POST' and 'y_pos' in request.form:
+            update.map_position_y = request.form['y_pos']
 
-    if request.method == 'POST' and 'piso' in request.form:
-        update.floor_id = request.form['piso']
+        if request.method == 'POST' and 'piso' in request.form:
+            update.floor_id = request.form['piso']
 
-    if request.method == 'POST' and 'info' in request.form:
-        update.info = request.form['info']
+        if request.method == 'POST' and 'info' in request.form:
+            update.info = request.form['info']
 
-    if request.method == 'POST' and 'type' in request.form:
-        update.room_type = request.form['type']
+        if request.method == 'POST' and 'type' in request.form:
+            update.room_type = request.form['type']
 
-    update.schedule_file = bd_doc
-    if request.method == 'POST' and schedule_file:
-        update.schedule_file = 'static/upload/' + files.save(request.files['horario'])
+        update.schedule_file = bd_doc
+        if request.method == 'POST' and schedule_file:
+            update.schedule_file = 'static/upload/' + files.save(request.files['horario'])
 
-    db.session.commit()
-    return redirect(url_for('list_sala'))
-
-
-@app.route('/admin')
-def admin():
-    return render_template('/admin/index.html')
+        db.session.commit()
+        return redirect(url_for('list_sala'))
+    return redirect(url_for('admin_login')) 
 
 
 @app.route('/admin_login')
@@ -283,14 +284,15 @@ def login():
     password = request.form['password']
     querydata = User.query.filter_by(email=email, password=password).first()
     session['username'] = querydata.nome
-    # session['profile'] = querydata.img
     return redirect(url_for('list_sala'))
+
 
 
 @app.route('/logout')
 def logout():
-    session.pop('username', None)
-    session.clear()
+    if 'username' in session:  
+        session.pop('username', None)
+        session.clear()
     return redirect(url_for('admin_login'))
 
 
@@ -307,9 +309,6 @@ def room_get():
     if(exists == False):
         return render_template('nosala.html')
     r = Room.query.filter_by(name=name).all()[0]
-
-    
-        #return "Sala não existe"
     return render_template(
         'index.html',
         id=r.id,
